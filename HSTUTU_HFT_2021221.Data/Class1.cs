64227bbe6c5cff5ca642a76456cc99416505a36d
TKornel23 <tkornel23@gmail.com>
2021-10-10 11:47:33 +0200

    1.1

---
+++ HSTUTU_HFT_2021221.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace HSTUTU_HFT_2021221.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ HSTUTU_HFT_2021221.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace HSTUTU_HFT_2021221.Logic
+{
+    public class Class1
+    {
+    }
+}
