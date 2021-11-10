62a1abf99fa052fd983886b41bfee919d3dbd4d7
TKornel23 <tkornel23@gmail.com>
2021-11-10 09:18:50 +0100

    BlogLogics

--- HSTUTU_HFT_2021221.Logic/BlogLogic.cs
+++ HSTUTU_HFT_2021221.Logic/BlogLogic.cs
@@ -1,4 +1,5 @@
 ﻿using HSTUTU_HFT_2021221.Models;
+using HSTUTU_HFT_2021221.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +8,28 @@ using System.Threading.Tasks;
 
 namespace HSTUTU_HFT_2021221.Logic
 {
-    public class BlogLogic
+    public class BlogLogic : IBlogLogic
     {
-        
+        IBlogRepository repo;
+
+        public BlogLogic(IBlogRepository repoPar)
+        {
+            this.repo = repoPar;
+        }
+
+        public void ChangeBlogTitle(int id, string title)
+        {
+            repo.ChangeBlogTitle(id, title);
+        }
+
+        public IList<Blog> GetAllBlogs()
+        {
+            return repo.GetAll().ToList<Blog>();
+        }
+
+        public Blog GetBlogById(int id)
+        {
+            return repo.GetOne(id);
+        }
     }
 }
