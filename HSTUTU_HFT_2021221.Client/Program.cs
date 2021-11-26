7c46197faa9293d1c9e1ebd08dbb9901853379c7
TKornel23 <tkornel23@gmail.com>
2021-11-26 15:58:25 +0100

    Rest Service

--- HSTUTU_HFT_2021221.Client/Program.cs
+++ HSTUTU_HFT_2021221.Client/Program.cs
@@ -1,97 +1,20 @@
-﻿using System;
+﻿using HSTUTU_HFT_2021221.Client;
+using HSTUTU_HFT_2021221.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HSTUTU_HFT_2021221
 {
-    class RestService
+    class Program
     {
-        HttpClient client;
-
-        public RestService(string baseurl)
+        static void Main(string[] args)
         {
-            Init(baseurl);
+            RestService rest = new RestService("http://localhost:54276");
+
+            var blogs = rest.Get<Blog>("blog");
+            var tags = rest.Get<Tag>("tag");
+            var posts = rest.Get<Post>("tag");
         }
-
-        private void Init(string baseurl)
-        {
-            client = new HttpClient();
-            client.BaseAddress = new Uri(baseurl);
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(
-                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue
-                ("application/json"));
-            try
-            {
-                client.GetAsync("").GetAwaiter().GetResult();
-            }
-            catch (HttpRequestException)
-            {
-                throw new ArgumentException("Endpoint is not available!");
-            }
-
-        }
-
-        public List<T> Get<T>(string endpoint)
-        {
-            List<T> items = new List<T>();
-            HttpResponseMessage response = client.GetAsync(endpoint).GetAwaiter().GetResult();
-            if (response.IsSuccessStatusCode)
-            {
-                items = response.Content.ReadAsAsync<List<T>>().GetAwaiter().GetResult();
-            }
-            return items;
-        }
-
-        public T GetSingle<T>(string endpoint)
-        {
-            T item = default(T);
-            HttpResponseMessage response = client.GetAsync(endpoint).GetAwaiter().GetResult();
-            if (response.IsSuccessStatusCode)
-            {
-                item = response.Content.ReadAsAsync<T>().GetAwaiter().GetResult();
-            }
-            return item;
-        }
-
-        public T Get<T>(int id, string endpoint)
-        {
-            T item = default(T);
-            HttpResponseMessage response = client.GetAsync(endpoint + "/" + id.ToString()).GetAwaiter().GetResult();
-            if (response.IsSuccessStatusCode)
-            {
-                item = response.Content.ReadAsAsync<T>().GetAwaiter().GetResult();
-            }
-            return item;
-        }
-
-        public void Post<T>(T item, string endpoint)
-        {
-            HttpResponseMessage response =
-                client.PostAsJsonAsync(endpoint, item).GetAwaiter().GetResult();
-
-            response.EnsureSuccessStatusCode();
-        }
-
-        public void Delete(int id, string endpoint)
-        {
-            HttpResponseMessage response =
-                client.DeleteAsync(endpoint + "/" + id.ToString()).GetAwaiter().GetResult();
-
-            response.EnsureSuccessStatusCode();
-        }
-
-        public void Put<T>(T item, string endpoint)
-        {
-            HttpResponseMessage response =
-                client.PutAsJsonAsync(endpoint, item).GetAwaiter().GetResult();
-
-
-            response.EnsureSuccessStatusCode();
-        }
-
     }
 }
